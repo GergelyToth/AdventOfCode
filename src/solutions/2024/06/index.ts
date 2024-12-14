57f1a29ed844a00bc69eb76b34dd2701c1991c1c
Gergely Toth <tgergely18@gmail.com>
2024-12-14 12:38:16 +0000

    day6: part 2

--- src/solutions/2024/06/index.ts
+++ src/solutions/2024/06/index.ts
@@ -15,7 +15,12 @@ const example = `....#.....
 
 const map = input.split("\n").map(x => x.split(""));
 
-const directionMap = {
+interface Cordinate {
+	x: number;
+	y: number;
+}
+
+const directionMap: {[direction: string]: Cordinate} = {
   'up': { x: 0, y: -1 },
   'right': { x: 1, y: 0 },
   'down': { x: 0, y: 1 },
@@ -23,12 +28,13 @@ const directionMap = {
 };
 let direction = 'up';
 
-const getNextDirection = (currentDirection) => {
+const getNextDirection = (currentDirection: string): string => {
 	const order = ['up', 'right', 'down', 'left'];
 	const currentIndex = order.findIndex(x => x === currentDirection);
 	return order[currentIndex + 1] || order[0];
 }
 
+// starting location
 let currentLocation = {
   x: map.find(row => row.includes('^'))!.findIndex(column => column === '^'),
   y: map.findIndex(row => row.includes('^')),
@@ -38,9 +44,12 @@ const maxColumns = map[0].length;
 
 // Helper functions
 const encodeCordinate = ({ x, y }) => `${x}x${y}`;
-const decodeCordinate = (coord) => {
+const decodeCordinate = (coord: string): Cordinate => {
   const [x, y] = coord.split('x');
-  return { x, y };
+  return { 
+		x: parseInt(x, 10), 
+		y: parseInt(y, 10),
+	};
 }
 
 const blocks: any = [];
@@ -50,34 +59,70 @@ map.forEach((row, rowIndex) => {
   });
 });
 
-const blocksHash = blocks.map(x => encodeCordinate(x));
+const calculateMovesetFromBlocks = (startLocation: Cordinate, blockedCords: Cordinate[], startDirection: string) => {
+	let collision = false;
+	let looping = false;
+	let direction = startDirection;
+	const moveHistory: { x: number, y: number }[] = [];
+	const moveHistoryHash: string[] = [];
+	let currentPosition = {...startLocation};
+	const blockedCordsHash = blockedCords.map(x => encodeCordinate(x));
 
-let collision = false;
-const moveHistory = [];
-// new implementation
-while (!collision) {
-	// check if we can move from the "currentLocation" in the faced "direction" by 1
-	const offset = directionMap[direction];
-	const nextPosition = {
-		x: currentLocation.x + offset.x,
-		y: currentLocation.y + offset.y,
-	};
+	while (!collision && !looping) {
+		// check if we can move from the "currentLocation" in the faced "direction" by 1
+		const offset = directionMap[direction];
+		const nextPosition = {
+			x: currentPosition.x + offset.x,
+			y: currentPosition.y + offset.y,
+		};
 
-	if (blocksHash.includes(encodeCordinate(nextPosition))) {
-		// turn 90 deg
-		direction = getNextDirection(direction);
-	} else if(nextPosition.x < 0 || nextPosition.x >= maxColumns || nextPosition.y < 0 || nextPosition.y >= maxRows) {
-		// if out of bounds, set collision to true
-		// still record the current position, the next one is that is not matching
-		moveHistory.push(currentLocation);
-		collision = true;
-	} else {
-		moveHistory.push(currentLocation);
-		currentLocation = nextPosition;
+		// check if we are looping
+		const currentMoveIndexInHistory = moveHistoryHash.findIndex(x => x === encodeCordinate(currentPosition));
+		if (currentMoveIndexInHistory !== -1 && moveHistoryHash[currentMoveIndexInHistory + 1] === encodeCordinate(nextPosition)) {
+			looping = true;
+			break;
+		}
+
+		if (blockedCordsHash.includes(encodeCordinate(nextPosition))) {
+			// turn 90 deg
+			direction = getNextDirection(direction);
+		} else if(nextPosition.x < 0 || nextPosition.x >= maxColumns || nextPosition.y < 0 || nextPosition.y >= maxRows) {
+			// if out of bounds, set collision to true
+			// still record the current position, the next one is that is not matching
+			moveHistory.push(currentPosition);
+			moveHistoryHash.push(encodeCordinate(currentPosition));
+			collision = true;
+		} else {
+			moveHistory.push(currentPosition);
+			moveHistoryHash.push(encodeCordinate(currentPosition));
+			currentPosition = nextPosition;
+		}
+	}
+
+	return {
+		looping,
+		collision,
+		moveHistory,
 	}
 }
 
-console.log(new Set([...moveHistory.map(x => encodeCordinate(x))]).size);
+// Part 1
+const part1Run = calculateMovesetFromBlocks(currentLocation, blocks, direction);
+const part1HistoryHash = new Set([...part1Run.moveHistory.map(x => encodeCordinate(x))]);
+console.log('part 1 answer:', part1HistoryHash.size);
 
-// TODO: Part 2
+// Part 2
+let loopingCounter = 0;
+let runCounter = 0;
+[...part1HistoryHash].map((newBlockHash: string) => {
+	const newBlockCord = decodeCordinate(newBlockHash);
+	// this takes a long time. Would be nice to multithread this, or find a better solution
+	console.log(runCounter++, `${Math.floor(100/part1HistoryHash.size * runCounter)}%`, loopingCounter);
+	const newRun = calculateMovesetFromBlocks(currentLocation, [...blocks, newBlockCord], direction);
+	if (newRun.looping) {
+		loopingCounter++;
+	}
+});
+
+console.log('part 2 answer:', loopingCounter);
 
